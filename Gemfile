3934d44e8cced0986b9dd8683bd82786b6b3ef1f
Anthony <wampirusy@gmail.com>
2017-11-19 19:11:59 +0200

    resize

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'omniauth-oauth2'
+
+gem "mini_magick"
--- app/models/command.rb
+++ app/models/command.rb
@@ -1,9 +1,22 @@
+require 'mini_magick'
 class Command < ApplicationRecord
   belongs_to :user
 
   def logo= file
+
+    image = MiniMagick::Image.read file.read
+    p '???', image.data
+    image.resize '200'
+    # image.crop '200x200+10+5'
+
     # p  '----',@file,super.@file,"-----#{file}------",self
     # @file = file.read
-    super Base64.encode64(file.read)
+    p '!!!!',  image
+
+    super Base64.encode64(image.to_blob)
+
+
+
+    # super Base64.encode64(file.read)
   end
 end
